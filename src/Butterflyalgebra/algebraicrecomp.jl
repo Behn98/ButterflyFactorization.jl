a70651e7414b646e76f5192e9b984fe4c68c2c2b
Behn98 <ben.merten@uni-rostock.de>
2026-05-02 17:32:13 +0200

    MultWIP

--- src/Butterflyalgebra/algebraicrecomp.jl
+++ src/Butterflyalgebra/algebraicrecomp.jl
@@ -6,6 +6,100 @@ function recompress_BF(Butterfly::BF2, τ)
     return recompress_BF_left(recompress_BF_right(Butterfly, τ), τ)
 end
 
+function recompress_BF_left(Butterfly::BF3, τ)
+    return recompress_BF_right(Butterfly', τ)'
+end
+
+function recompress_BF(Butterfly::BF3, τ)
+    return recompress_BF_left(recompress_BF_right(Butterfly, τ), τ)
+end
+
+function find_rows_for_column(R::Dict{T,Dict{T,Matrix}}, col_idx::Int) where {T}
+    rows = Vector{T}()
+    for (row, inner_dict) in R
+        if haskey(inner_dict, col_idx)
+            push!(rows, row)
+        end
+    end
+    return rows
+end
+
+function recompress_BF_right(Butterfly::BF3, τ)
+    Q = Butterfly.Q
+    R = Butterfly.R
+    P = Butterfly.P
+    NS = Butterfly.NS
+    NO = Butterfly.NO
+    k = Butterfly.k
+    τ2 = Butterfly.τ
+
+    # --- trees & helpers ---
+    H2Blocktree = Butterfly.tree
+
+    for l in eachindex(R)
+        lold = Butterfly.level - l + 1
+        R_u = Dict{Int,Dict{Int,Matrix{ComplexF64}}}()
+        col = Vector{Tuple{Int,Int}}(undef, 0)
+        for row in keys(R[lold])
+            unique(append!(col, keys(R[lold][row])))
+        end
+        for col_idx in col
+            rows_with_col = [
+                row for (row, inner_dict) in R[lold] if haskey(inner_dict, col_idx)
+            ]
+            R_k = Vector{Matrix{ComplexF64}}()
+            row_spc = Vector{Int}()
+            i = 1
+            for row in rows_with_col
+                push!(R_k, R[lold][row][col_idx])
+                push!(row_spc, size(R_k[i], 1))
+
+                i += 1
+            end
+            A_k = vcat(R_k...)
+            QRA = pqr(A_k; rtol=τ)
+            if !haskey(R_u, col_idx[1])
+                R_u[col_idx[1]] = Dict{Int,Matrix{ComplexF64}}()
+            end
+            R_u[col_idx[1]][col_idx[2]] = QRA[2][:, invperm(QRA[3])]
+            last = 0
+            j = 1
+            for row in rows_with_col
+                R[lold][row][col_idx] = QRA[1][(last + 1):(last + row_spc[j]), :]
+                last += row_spc[j]
+                j += 1
+            end
+        end
+        if l < Butterfly.level - 1
+            update_next_level_R_right!(R[lodl - 1], R_u)
+        else
+            update_next_level_R_right!(Q, R_u)
+        end
+    end
+
+    return BF3(Q, R, P, H2Blocktree, Butterfly.dim, Butterfly.level, NS, NO, k, τ2)
+end
+
+@views function update_next_level_R_right!(
+    rightfactor::Dict{Tuple{Int,Int},Dict{Tuple{Int,Int},AbstractMatrix{ComplexF64}}},
+    R_u::Dict{Int,AbstractMatrix{ComplexF64}},
+)
+    for row in keys(rightfactor)
+        for col in keys(rightfactor[row])
+            rightfactor[row][col] = R_u[row[1]][row[2]] * rightfactor[row][col]
+        end
+    end
+end
+
+@views function update_next_level_R_right!(
+    rightfactor::Dict{Int,AbstractMatrix{ComplexF64}},
+    R_u::Dict{Int,AbstractMatrix{ComplexF64}},
+)
+    for nodeS in treeS[LS]
+        Q[nodeS] = R_u[nodeS][NO] * Q[nodeS]
+    end
+end
+
 function recompress_BF_right(Butterfly::BF2, τ)
     Q = Butterfly.Q
     R = Butterfly.R
--- src/symbfmult.jl
+++ src/symbfmult.jl
@@ -1,24 +1,17 @@
-function Base.size(A::ButterflyFactorization.BF2, dim=nothing)
-    if dim === nothing
-        return (A.dim[1], A.dim[2])
-    elseif dim == 1
-        return A.dim[1]
-    elseif dim == 2
-        return A.dim[2]
-    else
-        error("dim must be either 1 or 2")
-    end
-end
-
-function Base.length(A::ButterflyFactorization.BF2)
-    return A.level + 1
-end
-
 @views function LinearAlgebra.mul!(
     y::AbstractVecOrMat, Butterfly::BF2, x::AbstractVector{T}
 ) where {T}
     LinearMaps.check_dim_mul(y, Butterfly, x)
     result = apply_BF2(Butterfly, x)
+    copyto!(y, result)
+    return nothing
+end
+
+@views function LinearAlgebra.mul!(
+    y::AbstractVecOrMat, Butterfly::BF3, x::AbstractVector{T}
+) where {T}
+    LinearMaps.check_dim_mul(y, Butterfly, x)
+    result = apply_BF3(Butterfly, x)
     copyto!(y, result)
     return nothing
 end
@@ -160,6 +153,72 @@ function apply_BF2(Butterfly::BF2, v::Vector{ComplexF64})
         inds = values(testT, Oleaf)
         dest = @view result[inds]
         mul!(dest, P[Oleaf], coefficients[NS][Oleaf])
+    end
+    return result
+end
+
+function apply_BF3(Butterfly::BF3, v::Vector{ComplexF64})
+    Q = Butterfly.Q
+    R = Butterfly.R
+    P = Butterfly.P
+    NO = Butterfly.NO
+    NS = Butterfly.NS
+    coefficients = Dict{Int,Dict{Int,Vector{ComplexF64}}}()
+    H2Blocktree = Butterfly.tree
+    trialT = H2Trees.trialtree(H2Blocktree)
+    testT = H2Trees.testtree(H2Blocktree)
+
+    values = H2Trees.values
+
+    # ------------------------------------------------------------
+    # Leaf initialization
+    # ------------------------------------------------------------
+    for Sleaf in keys(Q)
+        srcvals = values(trialT, Sleaf)
+        getsubdict!(coefficients, NO)[Sleaf] = Vector{ComplexF64}(undef, size(Q[Sleaf])[1])
+        @views mul!(coefficients[NO][Sleaf], Q[Sleaf], v[srcvals])
+    end
+
+    # Step 2: Sequentially apply R factors
+    for l in eachindex(R)
+        for (obs_child, src_node) in keys(R[l])
+            first = true
+            for (obs_node, src_child) in keys(R[l][(obs_child, src_node)])
+                if first
+                    getsubdict!(coefficients, obs_child)[src_node] = Vector{ComplexF64}(
+                        undef, size(R[l][(obs_child, src_node)][(obs_node, src_child)])[1]
+                    )
+                    @views mul!(
+                        coefficients[obs_child][src_node],
+                        R[l][(obs_child, src_node)][(obs_node, src_child)],
+                        coefficients[obs_node][src_child],
+                    )
+                    first = false
+                else
+                    coeff_temp = Vector{ComplexF64}(
+                        undef, size(R[l][(obs_child, src_node)][(obs_node, src_child)])[1]
+                    )
+                    @views mul!(
+                        coeff_temp,
+                        R[l][(obs_child, src_node)][(obs_node, src_child)],
+                        coefficients[obs_node][src_child],
+                    )
+                    getsubdict!(coefficients, obs_child)[src_node] += coeff_temp
+                end
+            end
+        end
+    end
+
+    # Step 3: Apply P to the result from the last R factor
+    # ------------------------------------------------------------
+    # Final assembly
+    # ------------------------------------------------------------
+    rootvals = values(testT, H2Trees.root(testT))
+    result = zeros(ComplexF64, length(rootvals))
+    for Oleaf in keys(P)
+        inds = values(testT, Oleaf)
+        dest = @view result[inds]
+        mul!(dest, P[Oleaf], coefficients[Oleaf][NS])
     end
     return result
 end
